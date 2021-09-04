4899e105e72abb4fcd87627e31e368c06b96889b
Bryce Colson <brycec17@vt.edu>
2021-09-03 17:03:04 -0700

    Add basic client

---
+++ src/main.py
@@ -0,0 +1,21 @@
+import socket
+
+class TCPClient:
+    host = "127.0.0.1"
+    port = 1866
+
+    def start(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.host, self.port))
+
+        print("Connected to", s.getsockname())
+
+        req = input("Enter req:")
+        s.sendall(str.encode(req))
+        res = s.recv(1024)
+        print(res.decode())
+        s.close()
+
+if __name__ == '__main__':
+    client = TCPClient()
+    client.start()
