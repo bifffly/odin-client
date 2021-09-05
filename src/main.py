a1ef9df9a553d972d0dd34f413862d4932a01360
Bryce Colson <brycec17@vt.edu>
2021-09-05 11:30:23 -0700

    Changed client architecture

--- src/main.py
+++ src/main.py
@@ -1,20 +1,35 @@
 import socket
 
-class TCPClient:
-    host = "127.0.0.1"
-    port = 1866
+while True:
+    req = input("> ").strip()
 
-    def start(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((self.host, self.port))
-        print("Connected to", s.getsockname())
+    if req.lower() == "q":
+        break
 
-        req = input("Enter req: ")
-        s.sendall(str.encode(req))
-        res = s.recv(4096)
-        print(res.decode())
-        s.close()
+    try:
+        while True:
+            s = socket.create_connection(("127.0.0.1", "1866"))
+            s.sendall(req.encode("UTF-8"))
 
-if __name__ == '__main__':
-    client = TCPClient()
-    client.start()
+            fp = s.makefile("rb")
+            header = fp.readline()
+            header = header.decode("UTF-8").strip()
+            version, status = header.split()
+            if status == "B":
+                print("Error code B: file not found")
+                break
+            elif status != "A":
+                print("Error code", status)
+                break
+            else:
+                break
+            break
+    except Exception as err:
+        print(err)
+        continue
+
+    if version == "rydja1":
+        body = fp.read()
+        body = body.decode("UTF-8")
+        print(body)
+
